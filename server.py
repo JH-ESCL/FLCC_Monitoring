2b874a0a59c4e0ae4f4c27a7ab74a079be3c1dbf
Jihun Song <134254888+JH-ESCL@users.noreply.github.com>
2024-03-08 13:17:05 +0900

    no ros

--- server.py
+++ server.py
@@ -2,10 +2,6 @@ import socket
 import sys
 import threading
 import define as d, max7318atg_code as m, crc16
-import matplotlib.pyplot as plt
-import pandas as pd
-import time
-import os
 import csv
 csv_filename = './log/data_log.csv'
 
@@ -139,60 +135,10 @@ def input_thread(m):
             print(f"An error occurred in the input thread: {e}")
 
 def plot_log():
-    plt.ion()  # Enable interactive mode
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 18))  # Create three subplots
-
     while True:
         try :             
             sendata = threading.Timer(0.1, sendData)
             sendata.start()
 
-            # Load the data from the CSV file
-            data = pd.read_csv(csv_filename)
-
-            # Clear existing data in plots
-            ax1.clear()
-            ax2.clear()
-            ax3.clear()
-
-            # Plot 'a' series data: assuming it's voltage and current data
-            for i in range(12):  # 12 pairs of voltage and current
-                ax1.plot(data[f'a.volt{i}'], label=f'Voltage {i} (V)')
-                ax1.plot(data[f'a.curr{i}'], label=f'Current {i} (A)', linestyle='--')
-
-            # Plot 'b' series data: adapt the number based on your specific data
-            ax2.plot(data['b.mpv1'], label='b.mpv1 (V)')
-            ax2.plot(data['b.mpc1'], label='b.mpc1 (A)')
-            ax2.plot(data['b.mpv2'], label='b.mpv2 (V)')
-            ax2.plot(data['b.mpc2'], label='b.mpc2 (A)')
-            # Add more plots for the 'b' series if needed
-
-            # Plot 'c' series data: adapt this part to your specific 'c' series data structure
-            ax3.plot(data['c.mux0'], label='c.mux0')
-            ax3.plot(data['c.mux1'], label='c.mux1')
-            ax3.plot(data['c.mux2'], label='c.mux2')
-            ax3.plot(data['c.mpmf1'], label='c.mpmf1')
-            # Add more plots for the 'c' series if needed
-
-            # Adding legends and setting labels
-            ax1.legend(loc='upper left')
-            ax1.set_title('Series a Data')
-            ax1.set_xlabel('Sample')
-            ax1.set_ylabel('Value')
-
-            ax2.legend(loc='upper left')
-            ax2.set_title('Series b Data')
-            ax2.set_xlabel('Sample')
-            ax2.set_ylabel('Value')
-
-            ax3.legend(loc='upper left')
-            ax3.set_title('Series c Data')
-            ax3.set_xlabel('Sample')
-            ax3.set_ylabel('Value')
-
-            # Use plt.pause to allow interactive updating. This can be adjusted or removed depending on your use case
-            plt.pause(0.1)
-
         except Exception as e:
             print(f"An error occurred in the plot and log: {e}")
-        time.sleep(1)  # Update interval; adjust as needed for your use case
